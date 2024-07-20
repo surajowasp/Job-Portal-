f472564a025ab4b9fa9245c7d8a2166d36b93546
surajowasp <surajkumarsah010@gmail.com>
2024-07-20 21:22:34 +0545

    initial commit

---
+++ index.php
@@ -0,0 +1,267 @@
+<?php
+// To Handle Session Variables on This Page
+session_start();
+
+// Including Database Connection From db.php file to avoid rewriting in all files
+require_once("db.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Job Portal</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="css/AdminLTE.min.css">
+  <link rel="stylesheet" href="css/_all-skins.min.css">
+  <!-- Custom -->
+  <link rel="stylesheet" href="css/custom.css">
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+</head>
+<body class="hold-transition skin-green sidebar-mini">
+<div class="wrapper">
+
+  <header class="main-header">
+    <!-- Logo -->
+    <a href="index.php" class="logo logo-bg">
+      <span class="logo-mini"><b>J</b>P</span>
+      <span class="logo-lg"><b>Job</b> Portal</span>
+    </a>
+
+    <!-- Header Navbar: style can be found in header.less -->
+    <nav class="navbar navbar-static-top">
+      <div class="navbar-custom-menu">
+        <ul class="nav navbar-nav">
+          <li>
+            <a href="jobs.php">Jobs</a>
+          </li>
+          <li> 
+            <a href="#about">About Us</a>
+          </li>
+          <?php if(empty($_SESSION['id_user']) && empty($_SESSION['id_company'])) { ?>
+          <li>
+            <a href="login.php">Login</a>
+          </li>
+          <li>
+            <a href="sign-up.php">Sign Up</a>
+          </li>  
+          <?php } else { 
+            if(isset($_SESSION['id_user'])) { 
+          ?>        
+          <li>
+            <a href="user/index.php">Dashboard</a>
+          </li>
+          <?php
+          } else if(isset($_SESSION['id_company'])) { 
+          ?>        
+          <li>
+            <a href="company/index.php">Dashboard</a>
+          </li>
+          <?php } ?>
+          <li>
+            <a href="logout.php" id="logout-link">Logout</a>
+          </li>
+          <?php } ?>
+        </ul>
+      </div>
+    </nav>
+  </header>
+
+  <div class="content-wrapper" style="margin-left: 0px;">
+    <section class="content-header bg-main">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center index-head">
+            <h1>All <strong>JOBS</strong> In One Place</h1>
+            <p>One search, global reach</p>
+            <p><a class="btn btn-success btn-lg" href="jobs.php" role="button">Search Jobs</a></p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section class="content-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 latest-job margin-bottom-20">
+            <h1 class="text-center">Latest Jobs</h1>            
+            <?php 
+          $sql = "SELECT * FROM job_post Order By Rand() Limit 4";
+          $result = $conn->query($sql);
+          if($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+              $sql1 = "SELECT * FROM company WHERE id_company='$row[id_company]'";
+              $result1 = $conn->query($sql1);
+              if($result1->num_rows > 0) {
+                while($row1 = $result1->fetch_assoc()) {
+            ?>
+            <div class="attachment-block clearfix">
+              <img class="attachment-img" src="img/photo1.png" alt="Attachment Image">
+              <div class="attachment-pushed">
+                <h4 class="attachment-heading"><a href="view-job-post.php?id=<?php echo $row['id_jobpost']; ?>"><?php echo $row['jobtitle']; ?></a> <span class="attachment-heading pull-right">$<?php echo $row['maximumsalary']; ?>/Month</span></h4>
+                <div class="attachment-text">
+                    <div><strong><?php echo $row1['companyname']; ?> | <?php echo $row1['city']; ?> | Experience <?php echo $row['experience']; ?> Years</strong></div>
+                </div>
+              </div>
+            </div>
+          <?php
+                }
+              }
+            }
+          }
+          ?>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="statistics" class="content-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center latest-job margin-bottom-20">
+            <h1>Our Statistics</h1>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-3 col-xs-6">
+            <div class="small-box bg-aqua">
+              <div class="inner">
+                <?php
+                  $sql = "SELECT * FROM job_post";
+                  $result = $conn->query($sql);
+                  $totalno = $result->num_rows > 0 ? $result->num_rows : 0;
+                ?>
+                <h3><?php echo $totalno; ?></h3>
+                <p>Job Offers</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-ios-paper"></i>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-xs-6">
+            <div class="small-box bg-green">
+              <div class="inner">
+                <?php
+                  $sql = "SELECT * FROM company WHERE active='1'";
+                  $result = $conn->query($sql);
+                  $totalno = $result->num_rows > 0 ? $result->num_rows : 0;
+                ?>
+                <h3><?php echo $totalno; ?></h3>
+                <p>Registered Company</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-briefcase"></i>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-xs-6">
+            <div class="small-box bg-yellow">
+              <div class="inner">
+                <?php
+                  $sql = "SELECT * FROM users WHERE resume!=''";
+                  $result = $conn->query($sql);
+                  $totalno = $result->num_rows > 0 ? $result->num_rows : 0;
+                ?>
+                <h3><?php echo $totalno; ?></h3>
+                <p>CV'S/Resume</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-ios-list"></i>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-xs-6">
+            <div class="small-box bg-red">
+              <div class="inner">
+                <?php
+                  $sql = "SELECT * FROM users WHERE active='1'";
+                  $result = $conn->query($sql);
+                  $totalno = $result->num_rows > 0 ? $result->num_rows : 0;
+                ?>
+                <h3><?php echo $totalno; ?></h3>
+                <p>Daily Users</p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-person-stalker"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="about" class="content-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 text-center latest-job margin-bottom-20">
+            <h1>About US</h1>                      
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <img src="img/browse.jpg" class="img-responsive">
+          </div>
+          <div class="col-md-6 about-text margin-bottom-20">
+            <p>
+            Welcome to Job Finder, your trusted partner in career discovery. We specialize in connecting talented individuals with top employers, making job searches seamless and efficient. Whether you're a job seeker looking for your dream job or an employer searching for the perfect candidate, we provide tailored solutions to meet your needs. Our platform features a wide range of opportunities across various industries, ensuring you find the right match. Join us at Job Finder and take the next step towards your future today!
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </div>
+  <!-- /.content-wrapper -->
+
+  <footer class="main-footer" style="margin-left: 0px;">
+    <div class="text-center">
+      <strong>Copyright &copy; 2024 <a href="learningfromscratch.online">Job Portal</a>.</strong> All rights reserved.
+    </div>
+  </footer>
+
+  <!-- /.control-sidebar -->
+  <div class="control-sidebar-bg"></div>
+
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery 3 -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<!-- AdminLTE App -->
+<script src="js/adminlte.min.js"></script>
+
+<!-- Custom JavaScript for logout confirmation -->
+<script>
+$(document).ready(function() {
+  $('#logout-link').click(function(event) {
+    event.preventDefault();
+    var logout = confirm("Are you sure you want to log out?");
+    if (logout) {
+      window.location.href = $(this).attr('href');
+    }
+  });
+});
+</script>
+</body>
+</html>
